5ce9e83c4d411bd32e596b54beed6cfeacd980c8
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-06-24 23:12:22 +0900

    Initial Commit

---
+++ DesignPatterns/DesignPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternsApp.swift
+//  DesignPatterns
+//
+//  Created by Hankyu Lee on 2022/06/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
