52d5f3561369253cb526fb7c404d168b205961ec
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-06-25 00:47:42 +0900

    Feat: API 호출 및 Data 변환

--- DesignPatterns/ContentView.swift
+++ DesignPatterns/ContentView.swift
@@ -7,10 +7,83 @@
 
 import SwiftUI
 
+//주석
+
 struct ContentView: View {
+    
+    @State var words:[String] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Rectangle()
+                .frame(height:0)
+                .background(.gray)
+                
+            Text("리스트 확인")
+                .padding()
+                .onTapGesture {
+                    WebService.getWords { words in
+                        self.words = words
+                    }
+                }
+                
+            Form {
+                ForEach(words, id:\.self) { word in
+                    Text(word)
+                }
+            }
+            
+        }
+        
+    }
+}
+
+enum Constants {
+    static let tenWordsURL = "https://random-word-api.herokuapp.com/word?number=10"
+}
+
+struct WordsViewModel {
+    
+    var words: [String] = []
+    
+    init(_ data: Data) {
+        words = transerDataToArray(data)
+    }
+
+    private func transerDataToArray(_ data:Data) -> [String] {
+        
+        let totalString = String(data: data, encoding: .utf8) ?? ""
+        let stringArray = totalString.split(separator: ",").map{String($0)}
+        
+        return stringArray.map{
+            $0.filter{$0.isLetter}
+        }
+    }
+    
+    var nWords: Int {
+        words.count
+    }
+    
+    var longWords: [String] {
+        words.filter{$0.count > 5}
+    }
+
+}
+
+enum WebService {
+    static func getWords(completion: @escaping ([String]) -> ()) {
+        let url = URL(string: Constants.tenWordsURL)!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil, let data = data else {
+                fatalError()
+            }
+            
+            let transfer = WordsViewModel(data)
+            
+            DispatchQueue.main.async {
+                completion(transfer.words)
+            }
+        }.resume()
     }
 }
 
